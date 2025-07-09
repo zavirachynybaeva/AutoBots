8814fe9f452e3b0c992a1ae4be4017cf8daf7161
zavira <chynyybaevaa@gmail.com>
2025-07-09 14:51:43 +0600

    version 1.0.2

--- src/main/java/ResetDemo.java
+++ src/main/java/ResetDemo.java
@@ -2,5 +2,6 @@ public class ResetDemo {
     public static void main(String[] args) {
         System.out.println("version 1.0.0");
         System.out.println("version 1.0.1");
+        System.out.println("version 1.0.2");
     }
 }
